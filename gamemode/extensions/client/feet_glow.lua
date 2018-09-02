42130a15393ebb2348b7b5990139026097ad8113
Callum J. Slaney <notq2f2@gmail.com>
2018-09-02 22:28:19 +0100

    More offset

--- gamemode/extensions/client/feet_glow.lua
+++ gamemode/extensions/client/feet_glow.lua
@@ -3,7 +3,7 @@ local ext = basewars.createExtension"feet-glow"
 local mat = Material"effects/select_ring"
 local rot = 0
 
-local off = Vector(0, 0, 0.01)
+local off = Vector(0, 0, 0.025)
 
 function ext:PrePlayerDraw(ply)
 	rot = rot + FrameTime() * 10 -- speed

f4956f5c739d65a30084dec3704f03581671e7a5
grmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2020-04-06 18:14:28 +0700

    create fonts in basicpanels

--- gamemode/libraries/moarpanels/basicpanels.lua
+++ gamemode/libraries/moarpanels/basicpanels.lua
@@ -47,3 +47,50 @@ function L(s,d,v,pnl)
 end
 
 Colors = Colors or {}
+
+
+local families = {
+	    ["Roboto"] = "R",
+	    ["Roboto Light"] = "RL",
+
+	    ["Titillium Web"] = "TW",
+	    ["Titillium Web SemiBold"] = "TWB",
+
+	    ["Open Sans"] = "OS",
+	    ["Open Sans SemiBold"] = "OSB",
+	    ["Open Sans Light"] = "OSL",
+
+	    ["Arial"] = "A",
+	    ["Helvetica"] = "HL",
+
+	    ["Montserrat"] = "MR",
+	    ["Montserrat Medium"] = "MRM",
+	    ["Montserrat-Bold"] = "MRB",	--bruh.....
+	    --["Montserrat SemiBold"] = "MRSB",
+
+	    ["SnareDrum Zero NBP"] = "SDZ",
+	    ["SnareDrum Two NBP"] = "SDT",
+
+	    ["BreezeSans"] = "BS",
+	    ["BreezeSans Medium"] = "BSSB",
+	    ["BreezeSans Light"] = "BSL",
+	    ["BreezeSans Bold"] = "BSB",
+
+	    ["DejaVu Sans"] = "DV",
+	}
+
+	FontFamilies = families
+
+	local sizes = {12, 14, 16, 18, 20, 24, 28, 32, 36, 48, 64, 72, 128}
+
+	for k,v in pairs(families) do 
+
+	    for _, size in pairs(sizes) do
+	        surface.CreateFont(v .. size, {
+	            font = k,
+	            size = size,
+	            weight = 400,
+	        })
+	    end
+
+	end
